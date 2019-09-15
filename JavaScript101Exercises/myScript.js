20d5bd5c9ce74ebe9779bc9ea0526b21182ca5aa
Kevin Capule <kapoolay@gmail.com>
2019-09-14 20:02:57 -0500

    adding Adams JS101

---
+++ JavaScript101Exercises/myScript.js
@@ -0,0 +1,39 @@
+function myFunction(something1, someting2) {
+    //objects
+    /*
+        let person = {
+            firstName : 'Kevin',
+            lastName : 'Capule',
+        }
+    
+        console.log(person.firstName);
+        console.log(person['lastName'],person['firstName']);
+    //arrays
+        let myArray = ['hello', 4]
+        console.log(myArray[0]);
+    
+    //numbers
+        let myNum = 3;
+        console.log(myNum);
+    
+        //myNum = myNum + 2;
+        myNum += 2;
+        console.log(myNum);
+    
+    
+    //.indexOf(sub) will return the index of the substring
+        let myString = 'Hello World';
+        let myIndex = myString.indexOf('World');
+        console.log(myIndex);
+    */
+    
+    //boolean
+        let myBooleanVariable = true;
+        let myOtherBoolean = false;
+        let result = myBooleanVariable && myOtherBoolean;
+        console.log(result);
+    }
+    
+    myFunction();
+    
+    
