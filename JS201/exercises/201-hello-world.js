72b82f7cbfad7306afee131d4761bc808d6d888d
Kevin Capule <kapoolay@gmail.com>
2019-09-18 00:47:49 -0500

    Updating to allow all folders to be accessible on GitHub

---
+++ JS201/exercises/201-hello-world.js
@@ -0,0 +1,20 @@
+// Write a function "hello" that takes one argument (a name), and returns a
+// string that says hello to the name.
+// Example: hello('Mustache') should return 'Hello, Mustache!'
+// Hey Flex Class, it'd s Patrick
+// Great question
+
+function hello(name) {
+    return 'Hello, ' + name + '!';
+}
+
+hello('Kevin');
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Write a function "helloDefault" such that if no name is given it will return
+// 'Hello, world!'
+// Otherwise it behaves the same as the "hello" function.
+
+function helloDefault(name = 'world') {
+    return 'Hello, ' + name + '!';
+}
