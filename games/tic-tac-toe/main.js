855495df7b7b10621ef98e77cc6c72fb8b022c4f
Kevin Capule <kapoolay@gmail.com>
2019-10-24 20:07:36 -0500

    finished tic-tac-toe

--- games/tic-tac-toe/main.js
+++ games/tic-tac-toe/main.js
@@ -30,18 +30,26 @@ function buttonClicked(event) {
     let columnIndex = clickedButtonId[2];
 
     //update the state
-    if (isPlayerXTurn) {
-        ticTacToe[rowIndex][columnIndex] = 'X';
-        isPlayerXTurn = false;
-    } else {
-        // it is "O's" Turn
-        ticTacToe[rowIndex][columnIndex] = 'O';
-        isPlayerXTurn = true;
+    if (getCurrentHTML(rowIndex, columnIndex) === "-") {
+        let idPlayer = document.getElementById('playerTurn');
+        if (isPlayerXTurn) {
+            ticTacToe[rowIndex][columnIndex] = 'X';
+            isPlayerXTurn = false;
+            idPlayer.innerHTML = "It's player X's turn!";
+            winner();
+        } else {
+            // it is "O's" Turn
+            ticTacToe[rowIndex][columnIndex] = 'O';
+            isPlayerXTurn = true;
+            idPlayer.innerHTML = "It's player O's turn!";
+            winner();
+        }
+    
+        renderCell(rowIndex, columnIndex);
+        
+    
+        console.log(clickedButtonId);
     }
-
-    renderCell(rowIndex, columnIndex);
-
-    console.log(clickedButtonId);
 }
 
 
@@ -57,9 +65,65 @@ function renderCell(row, column) {
     console.log(button);
 }
 
+function getCurrentHTML(row, column) {
+    let id = `${row},${column}`;
+
+    let button = document.getElementById(id);
+    return button.innerHTML;
+}
+
+// function playerTurn() {
+//     let id = document.getElementById('playerTurn');
+//     if (isPlayerXTurn) {
+//         id.innerHTML = "It's player X's turn!";
+//     } else {
+//         id.innerHTML = "It's player O's turn!";
+//     }
+// }
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~WINNING CONDITIONS~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+function winner () {
+    if (
+        // row
+        ticTacToe[0][0]+ticTacToe[0][1]+ticTacToe[0][2] === 'XXX' ||
+        ticTacToe[1][0]+ticTacToe[1][1]+ticTacToe[1][2] === 'XXX' ||
+        ticTacToe[2][0]+ticTacToe[2][1]+ticTacToe[2][2] === 'XXX' ||
+        // columns
+        ticTacToe[0][0]+ticTacToe[1][0]+ticTacToe[2][0] === 'XXX' ||
+        ticTacToe[0][1]+ticTacToe[1][1]+ticTacToe[2][1] === 'XXX' ||
+        ticTacToe[0][2]+ticTacToe[1][2]+ticTacToe[2][2] === 'XXX' ||
+        // diagonal
+        ticTacToe[0][0]+ticTacToe[1][1]+ticTacToe[2][2] === 'XXX' ||
+        ticTacToe[0][2]+ticTacToe[1][1]+ticTacToe[2][0] === 'XXX' ) {
+            document.getElementById('winner').innerHTML ='Player X Wins!';
+    } 
+    if (
+        // row
+        ticTacToe[0][0]+ticTacToe[0][1]+ticTacToe[0][2] == 'OOO' ||
+        ticTacToe[1][0]+ticTacToe[1][1]+ticTacToe[1][2] === 'OOO' ||
+        ticTacToe[2][0]+ticTacToe[2][1]+ticTacToe[2][2] === 'OOO' ||
+        // columns
+        ticTacToe[0,0]+ticTacToe[1,0]+ticTacToe[2][0] === 'OOO' ||
+        ticTacToe[0,1]+ticTacToe[1,1]+ticTacToe[2][1] === 'OOO' ||
+        ticTacToe[0,2]+ticTacToe[1,2]+ticTacToe[2][2] === 'OOO' ||
+        // diagonal
+        ticTacToe[0][0]+ticTacToe[1][1]+ticTacToe[2][2] === 'OOO' ||
+        ticTacToe[0][2]+ticTacToe[1][1]+ticTacToe[2][0] === 'OOO' ) {
+            document.getElementById('winner').innerHTML ='Player O Wins!';
+    } 
+}
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// ~~~~~~~~RESET~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+function reset () {
+    document.location.reload();
+}
 
 
 
@@ -71,7 +135,7 @@ function renderGame(game) {
 
     return `
         <div class="container d-flex flex-column justify-content-start align-items-center">
-            <h4>It's player O's turn!</h4>
+            <h4 id='playerTurn'>It's player O's turn!</h4>
             <div class="w-50 text-center">
                 <button id='0,0' onClick="buttonClicked(event)">${game[0][0]}</button>
                 <button id='0,1' onClick="buttonClicked(event)">${game[0][1]}</button>
@@ -87,6 +151,8 @@ function renderGame(game) {
                 <button id='2,1' onClick="buttonClicked(event)">${game[2][1]}</button>
                 <button id='2,2' onClick="buttonClicked(event)">${game[2][2]}</button>
             </div>
+            <h4 id='winner'></h4>
+            <button onClick='reset()' style="width:100px;">Reset</button>
         </div>
     `
 }
