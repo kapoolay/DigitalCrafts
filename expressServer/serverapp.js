f65d8dfd6da921b49905fc4e588b02bf773553f0
Kevin Capule <kapoolay@gmail.com>
2019-11-24 14:53:22 -0600

    updated/working serverapp.js

--- expressServer/serverapp.js
+++ expressServer/serverapp.js
@@ -6,6 +6,10 @@ const serverApp = express();
 
 serverApp.use(express.static(__dirname + '/public'));       // "__dirname" is the direct path name of the serverapp location
 
+serverApp.get('/myapi/hello', function(request, response, next) {
+    response.send('Helloooooooooo');
+});
+
 serverApp.listen(3000, function() {
     console.log('My server is listening on port 3000');
 });
