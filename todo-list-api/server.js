0643d241c29f286f155f2b8e333c33b47dd61c47
Kevin Capule <kapoolay@gmail.com>
2019-12-03 21:12:52 -0600

    todo-list-api completed

--- todo-list-api/server.js
+++ todo-list-api/server.js
@@ -1,44 +1,88 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-var app = express();
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-var todoList = [
-    {
-        id: 1,
-        todo: "Implement a REST API"
-    }
+let counter = 1;
+let todoList = [
+  {
+    id: 1,
+    todo: "Implement a REST API"
+  }
 ];
 
 // GET /api/todos
-app.get('/api/todos', function(req, res, next) {
-    console.log(todoList);
-    res.send(todoList);
+app.get("/api/todos", function (request, response, next) {
+  response.json(todoList);
 });
 
 // GET /api/todos/:id
-app.get('/api/todos/:id', function(req, res, next) {
-    res.send(todoList[req.params.id])
-})
+app.get("/api/todos/:id", function (request, response, next) {
+  let todoItem = todoList.find(function (element) {
+    return element.id.toString() === request.params.id;
+  });
+
+  response.json(todoItem);
+});
 
 // POST /api/todos
-app.post('/api/todos', function(req, res, next) {
-    res.send('Post is working');
-    console.log('trying to post');
+app.post("/api/todos", function (request, response, next) {
+  counter++;
+
+  let todoItem = request.body;
+  /*
+  todoIteem = {
+    key1: value1,
+    key2: value2,
+    key3: value3,
+    key4: value4
+  }
+  */
+  todoItem = { id: counter, ...todoItem }; // note we are using the spread operator (...)
+
+  todoList.push(todoItem);
+
+  response.json(todoItem);
 });
 
 // PUT /api/todos/:id
-app.put('/api/todos/:id', function(req, res, next) {
+app.put("/api/todos/:id", function (request, response, next) {
+  let todoItem = todoList.find(function (element) {
+    return element.id.toString() === request.params.id;
+  });
 
-})
+  //   let newPropertyEntries = Object.entries(request.body)[0]; // just the first key/value pair
+  //   todoItem[newPropertyEntries[0]] = newPropertyEntries[1];
+
+  if (typeof todoItem == 'undefined') {
+    response.json('you F up');
+  }
+  
+  var newTodoData = request.body;
+  var newTodoKeys = Object.keys(newTodoData); // ["todo", "todo2"]
+
+  newTodoKeys.forEach(function (key) {
+    todoItem[key] = newTodoData[key];
+  });
+
+  response.json(todoItem);
+});
 
 // DELETE /api/todos/:id
-app.delete('/api/todos/:id', function(req, res, next) {
-    
-})
+app.delete("/api/todos/:id", function (request, response, next) {
+  let indexToRemove = todoList.findIndex(function (todoItem) {
+    return todoItem.id.toString() === request.params.id;
+  });
 
-app.listen(3000, function(){
-    console.log('Todo List API is now listening on port 3000...');
-})
+  if (indexToRemove >= 0) {
+    todoList.splice(indexToRemove, 1);
+  }
+
+  response.sendStatus(200);
+});
+
+app.listen(3000, function () {
+  console.log("Todo List API is now listening on port 3000...");
+});
