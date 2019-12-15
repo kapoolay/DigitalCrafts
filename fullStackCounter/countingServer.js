af66b6f2818bb83bc69c6ca7dc178741a6830cea
Kevin Capule <kapoolay@gmail.com>
2019-12-15 17:13:10 -0600

    fullStackCounter completed

--- fullStackCounter/countingServer.js
+++ fullStackCounter/countingServer.js
@@ -4,58 +4,58 @@ const promise = require('bluebird');
 
 // pg-promise initialization options:
 const initOptions = {
-    // Use a custom promise library, instead of the default ES6 Promise:
-    promiseLib: promise, 
+  // Use a custom promise library, instead of the default ES6 Promise:
+  promiseLib: promise, 
 };
 
 // Database connection parameters:
 const config = {
-    host: 'localhost',
-    port: 5432,
-    database: 'fullstackcounter',
-    user: 'postgres'
+  host: 'localhost',
+  port: 5432,
+  database: 'fullstackcounter',
+  user: 'postgres',
 };
 
-// Load and initialize pg-promise:
 const pgp = require('pg-promise')(initOptions);
-
-// Create the database instance:
 const db = pgp(config);
-
-// function handleQuery() {
-//   db.query();
-//   pgp.end();
-// }
 
 app.use(express.static(__dirname + '/public'));
 
 
 app.get("/value", function (req, response){
-db.query('SELECT SUM(current_count) AS count_sum FROM counter')
-  .then(function(result) {
+  db.one("SELECT SUM(current_count) AS count_sum FROM counter")
+  .then(function (result) {
     console.log(result);
-    response.send(result.current_sum);
+    response.send(result.count_sum);
   });
 });
 
 app.get("/increment", function (req, response){
-  db.result(`INSERT INTO counter('current_count') VALUES(1)`)
-  .then(function(result) {
+  db.one(`INSERT INTO counter("current_count") VALUES(1); 
+  SELECT SUM(current_count) AS count_sum FROM counter`)
+  .then(function (result){
     console.log(result);
-    response.send(result);
+    response.send(result.count_sum);
   });
 });
 
-// app.get("/reset", function (req, response){
-//   counter = 0;
-//   response.send(counter.toString());
-// });
+app.get("/reset", function (req, response) {
+  db.result(`INSERT INTO counter(current_count) 
+  VALUES (0 - (SELECT SUM(current_count) AS count_sum FROM counter))`)
+  .then(function (result){
+    response.json(0);
+  });
+});
 
 
-// app.get("/decrease", function (req, response){
-//   counter--;
-//   response.send(counter.toString());
-// });
+app.get("/decrement", function (req, response){
+  db.one(`INSERT INTO counter("current_count") VALUES(-1); 
+  SELECT SUM(current_count) AS count_sum FROM counter`)
+  .then(function (result){
+    console.log(result);
+    response.send(result.count_sum);
+  });
+});
 
 app.listen(3000, function(){
   console.log("server listening on post 3000");
