7f8df0124bcf3fad1bf5dff3dacc1808be49fe56
Kevin Capule <kapoolay@gmail.com>
2019-12-19 20:29:39 -0600

    music sequelize done

--- music_sequel_ize/list_artists.js
+++
@@ -1,8 +0,0 @@
-const models = require('./models');
-
-models.artist.findAll({ attributes: ['name'], include: ['albums']}).then(function (artists) {
-  artists.forEach(element => {
-    console.log(element.name);
-    console.log(element.albums);
-  });
-});
